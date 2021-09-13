b5d57161a6dd9e8032471dd369690c8d74a95079
Benjamin <mafortion.benjamin@gmail.com>
2021-09-13 16:35:07 +0200

    Infinite scrolling background

--- Assets/Scripts/BackgroundGrid.cs
+++ Assets/Scripts/BackgroundGrid.cs
@@ -4,12 +4,18 @@ using UnityEngine;
 
 public class BackgroundGrid : MonoBehaviour
 {
+    public Transform objectToFollow;
+    public Vector2 repeatPeriod = new Vector2(10.0f, 10.0f);
+
     void Start()
     {
         
     }
-    void Update()
+    void LateUpdate()
     {
-        
+        Vector2 pos = objectToFollow.position;
+        pos.x -= Mathf.Repeat(pos.x, repeatPeriod.x);
+        pos.y -= Mathf.Repeat(pos.y, repeatPeriod.y);
+        transform.position = pos;
     }
 }
